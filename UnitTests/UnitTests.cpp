4cb52fc3692b72814076a372e2b9c2bb00006886
Paul Manning <psmanningjr@gmail.com>
2024-04-30 14:41:36 -0600

    Add dllnode class tests

---
+++ UnitTests/UnitTests.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTests
+{
+	TEST_CLASS(UnitTests)
+	{
+	public:
+		
+		TEST_METHOD(TestDoubleLinkedListNode)
+		{
+			DoubleLinkedListNode dllNode;
+		}
+	};
+}
